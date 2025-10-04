cdbd3039dfce7f68afba39e31e0fb6aac72e789b
vrishabhparmar <vrishabhluvcars@gmail.com>
2025-10-04 23:03:00 +0530

    REST API

--- foundation/simple-rest-api/index.js
+++ foundation/simple-rest-api/index.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 
 const app = express()
 
-app.use(express.json()); 
+app.use(express.json()); // Middleware which allows parseing JSON objects
 
-mongoose.connect(process.env.MONDODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB'));
@@ -31,6 +31,29 @@ const taskSchema = new mongoose.Schema({
 const Task = mongoose.model('Task', taskSchema);
 
 // Routes 
+app.get('/', (req, res) => {
+    res.json({
+        "msg":"Hello From server"
+    })
+})
+
+app.get('/tasks', async (req , res) => {
+   // const {page = 1, limit = 10, sortBy = 'createdAt', order = 'desc'} = req.query;
+
+   const tasks = await Task.find();
+
+   const total = await Task.countDocuments();
+   res.json({tasks})
+
+})
+
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json(task);
+})
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
